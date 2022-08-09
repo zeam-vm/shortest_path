57e53e88f93b24739894404dd8b4a2909c7012aa
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-09 22:18:17 +0900

    set version to 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShortestPath.MixProject do
   def project do
     [
       app: :shortest_path,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.14-rc",
       start_permanent: Mix.env() == :prod,
       deps: deps()

f8ad424f6961497b31af4b16e41e8622b11310b7
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-10 13:12:06 +0900

    remove redundant test/support/out

--- test/shortest_path/dijkstra/main_a_test.exs
+++ test/shortest_path/dijkstra/main_a_test.exs
@@ -7,12 +7,24 @@ defmodule ShortestPath.Dijkstra.MainATest do
     {n, m, inputs} = ShortestPath.Dijkstr.InputReader.read("sample_01.txt")
     outputs = MainA.main(n, m, inputs)
 
-    answer =
+    actual =
       outputs
+      |> Enum.with_index()
+      |> Enum.map(fn {ds, i} ->
+        ds
+        |> Enum.with_index()
+        |> Enum.filter(fn {_d, j} -> i < j end)
+        |> Enum.map(fn {d, _j} -> d end)
+      end)
       |> Enum.map(& Enum.join(&1, " "))
+      |> Enum.reject(& &1 == "")
       |> Enum.join("\n")
 
-    assert answer == File.read!("test/support/out/sample_01.txt")
+    expected =
+      File.read!("test/support/out/sample_01.txt")
+      |> String.trim()
+
+    assert actual == expected
   end
 
 end

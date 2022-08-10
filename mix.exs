5443f035ad5d724dd3b489e8e3813800f1d98859
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-11 06:35:23 +0900

    revert version to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ShortestPath.MixProject do
     [
       app: :shortest_path,
       version: "0.1.0-dev",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14-rc",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

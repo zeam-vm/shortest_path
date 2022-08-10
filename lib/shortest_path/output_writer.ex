dccd9fb3619a2ece7c65062aed926d549ad40d53
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-10 18:34:56 +0900

    apply mix format

--- lib/shortest_path/output_writer.ex
+++ lib/shortest_path/output_writer.ex
@@ -8,8 +8,8 @@ defmodule ShortestPath.Dijkstr.OutputWriter do
       |> Enum.filter(fn {_d, j} -> i < j end)
       |> Enum.map(fn {d, _j} -> d end)
     end)
-    |> Enum.map(& Enum.join(&1, " "))
-    |> Enum.reject(& &1 == "")
+    |> Enum.map(&Enum.join(&1, " "))
+    |> Enum.reject(&(&1 == ""))
     |> Enum.join("\n")
   end
 end

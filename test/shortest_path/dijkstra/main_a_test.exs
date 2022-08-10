3328e0bf6eed8ceeda6c6ca975cab8173d88dd86
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-10 13:16:13 +0900

    extract output_writer

--- test/shortest_path/dijkstra/main_a_test.exs
+++ test/shortest_path/dijkstra/main_a_test.exs
@@ -5,20 +5,10 @@ defmodule ShortestPath.Dijkstra.MainATest do
 
   test "solve case1 by Dijkstra" do
     {n, m, inputs} = ShortestPath.Dijkstr.InputReader.read("sample_01.txt")
-    outputs = MainA.main(n, m, inputs)
 
     actual =
-      outputs
-      |> Enum.with_index()
-      |> Enum.map(fn {ds, i} ->
-        ds
-        |> Enum.with_index()
-        |> Enum.filter(fn {_d, j} -> i < j end)
-        |> Enum.map(fn {d, _j} -> d end)
-      end)
-      |> Enum.map(& Enum.join(&1, " "))
-      |> Enum.reject(& &1 == "")
-      |> Enum.join("\n")
+      MainA.main(n, m, inputs)
+      |> ShortestPath.Dijkstr.OutputWriter.puts()
 
     expected =
       File.read!("test/support/out/sample_01.txt")

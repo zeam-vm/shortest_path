56b1188743db8e0b2de9d972401b24b9ec8ff4e3
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-11 08:38:57 +0900

    set up ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule ShortestPath.MixProject do
       version: "0.1.0-dev",
       elixir: "~> 1.14-rc",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "ShortestPath",
+      source_url: "https://github.com/zeam-vm/shortest_path",
+      docs: [
+        main: "README",
+        before_closing_body_tag: &before_closing_body_tag/1,
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,4 +35,15 @@ defmodule ShortestPath.MixProject do
       {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
+
+  defp before_closing_body_tag(:html) do
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.css" integrity="sha384-beuqjL2bw+6DBM2eOpr5+Xlw+jiH44vMdVQwKxV28xxpoInPHTVmSvvvoPq9RdSh" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.js" integrity="sha384-aaNb715UK1HuP4rjZxyzph+dVss/5Nx3mLImBe9b0EW4vMUkc1Guw4VRyQKBC0eG" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
+            onload="renderMathInElement(document.body);"></script>
+    """
+  end
+
+  defp before_closing_body_tag(_), do: ""
 end

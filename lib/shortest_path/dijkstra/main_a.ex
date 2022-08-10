c0420df4f50f8b93cd5cde1f8f3459fd02915649
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-10 18:33:57 +0900

    apply mix format

--- lib/shortest_path/dijkstra/main_a.ex
+++ lib/shortest_path/dijkstra/main_a.ex
@@ -7,18 +7,17 @@ defmodule ShortestPath.Dijkstra.MainA do
     node_map =
       vicinities
       |> Enum.reduce(%{}, fn [s, d, w], acc ->
-        Map.update(acc, s, %{d => w}, & Map.put(&1, d, w))
+        Map.update(acc, s, %{d => w}, &Map.put(&1, d, w))
         # 逆方向分も追加
-        |> Map.update(d, %{s => w}, & Map.put(&1, s, w))
+        |> Map.update(d, %{s => w}, &Map.put(&1, s, w))
       end)
 
     # 各ノードにダイクストラ法を実行
     for start_node <- 1..n do
-
       # 探索済みノード
       # 出発地点は距離0, それ以外は距離INF
       acc =
-        (for i <- 1..n, into: %{}, do: {i, @inf})
+        for(i <- 1..n, into: %{}, do: {i, @inf})
         |> Map.put(start_node, 0)
 
       # 未探索ノード
@@ -28,17 +27,17 @@ defmodule ShortestPath.Dijkstra.MainA do
     end
   end
 
-
   @spec dijkstra(list(), map(), map()) :: list()
   def dijkstra(unsearched, node_map, acc)
 
   def dijkstra([], _, acc), do: acc |> Map.values()
+
   def dijkstra(unsearched, node_map, acc) do
     # 未探索ノードの中から最短のものを取得
     [{node, w} | unsearched] = Enum.sort_by(unsearched, &elem(&1, 1))
 
     # 同じノードで最短以外のものがあれば除去しておく
-    unsearched = unsearched |> Enum.reject(& elem(&1, 0) == node)
+    unsearched = unsearched |> Enum.reject(&(elem(&1, 0) == node))
 
     # 取得したノードに隣接するノードを検索し、
     new_unsearched =
@@ -49,10 +48,10 @@ defmodule ShortestPath.Dijkstra.MainA do
 
     # 確定ノードを更新
     acc =
-        new_unsearched
-        |> Enum.reduce(acc, fn {nn, ww}, acc ->
-          Map.put(acc, nn, ww)
-        end)
+      new_unsearched
+      |> Enum.reduce(acc, fn {nn, ww}, acc ->
+        Map.put(acc, nn, ww)
+      end)
 
     # IO.inspect({node, w}, label: "取得したノード")
     # IO.inspect(unsearched ++ new_unsearched, label: "未探索ノード")

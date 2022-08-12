bceaac7217a6e817336bcbd8922760851751252f
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-12 21:59:13 +0900

    fix an issue of types

--- lib/shortest_path/solver_from_weighted_edge_list.ex
+++ lib/shortest_path/solver_from_weighted_edge_list.ex
@@ -1,6 +1,6 @@
 defmodule ShortestPath.SolverFromWeightedEdgeList do
-  @type weighted_edge_list :: list(list(pos_integer()))
-  @type weight_between_nodes_list :: list(list(pos_integer()))
+  @type weighted_edge_list() :: list(list(pos_integer()))
+  @type weight_between_nodes_list() :: list(list(pos_integer()))
 
   @callback main(pos_integer(), pos_integer(), weighted_edge_list) :: weight_between_nodes_list
 

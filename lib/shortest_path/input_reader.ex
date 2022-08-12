d8b0db35598ac6dc47eac6c289d5d900e3f40db8
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-12 18:41:20 +0900

    modify input_reader to use weighted_edge_list

--- lib/shortest_path/input_reader.ex
+++ lib/shortest_path/input_reader.ex
@@ -5,7 +5,7 @@ defmodule ShortestPath.InputReader do
   @doc """
   Reads a given file and returns a tuple of *N*, *M* and the list of the lists `[Vaj, Vbj, Wj]` in `test/support/in`.
   """
-  @spec read(Path.t()) :: {pos_integer(), pos_integer(), list(list(pos_integer()))}
+  @spec read(Path.t()) :: {pos_integer(), pos_integer(), ShortestPath.SolverFromWeightedEdgeList.weighted_edge_list()}
   def read(file) do
     read_directly(Path.join(@support_in, file))
   end
@@ -13,7 +13,7 @@ defmodule ShortestPath.InputReader do
   @doc """
   Reads a given file and returns a tuple of *N*, *M* and the list of the lists `[Vaj, Vbj, Wj]` in anywhere in the file system.
   """
-  @spec read_directly(Path.t()) :: {pos_integer(), pos_integer(), list(list(pos_integer()))}
+  @spec read_directly(Path.t()) :: {pos_integer(), pos_integer(),  ShortestPath.SolverFromWeightedEdgeList.weighted_edge_list()}
   def read_directly(file) do
     stream = File.stream!(file, [read_ahead: @buffer_size], :line)
 

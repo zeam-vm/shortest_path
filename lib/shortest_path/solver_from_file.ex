49732bec19a06ea242ed888cae166eec655ad897
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-13 02:53:32 +0900

    add main_pp

--- lib/shortest_path/solver_from_file.ex
+++ lib/shortest_path/solver_from_file.ex
@@ -7,4 +7,12 @@ defmodule ShortestPath.SolverFromFile do
   Returns a string of the result weights between nodes from an input case file.
   """
   @callback main_p(Path.t(), module()) :: String.t()
+
+  @doc """
+  Returns a string of the result weights between nodes from an input case file.
+  """
+  @spec main_pp(Path.t(), module(), module()) :: String.t()
+  def main_pp(file, module, solver_module) do
+    Function.capture(solver_module, :main_p, 2).(file, module)
+  end
 end

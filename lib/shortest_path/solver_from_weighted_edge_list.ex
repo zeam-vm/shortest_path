12c74cd6123c0371b4126bc30fbef94fd4b17e5c
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-12 19:14:15 +0900

    create main_p

--- lib/shortest_path/solver_from_weighted_edge_list.ex
+++ lib/shortest_path/solver_from_weighted_edge_list.ex
@@ -3,4 +3,16 @@ defmodule ShortestPath.SolverFromWeightedEdgeList do
   @type weight_between_nodes_list :: list(list(pos_integer()))
 
   @callback main(pos_integer(), pos_integer(), weighted_edge_list) :: weight_between_nodes_list
+
+  @spec main_p(Path.t(), module()) :: String.t()
+  def main_p(file, module) do
+    if function_exported?(module, :main, 3) do
+      {n, m, inputs} = ShortestPath.InputReader.read_directly(file)
+
+      Function.capture(module, :main, 3).(n, m, inputs)
+      |> ShortestPath.OutputWriter.puts()
+    else
+      raise RuntimeError, "#{inspect module} isn't an implementation of ShortestPath.SolverFromWeightedEdgeList."
+    end
+  end
 end
